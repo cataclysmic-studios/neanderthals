149483ef74e9ac2efd04892ab0eefe0d717ca114
R-unic <roleyspersonal@gmail.com>
2025-07-10 11:55:33 -0400

    fix: proper data initialization

--- src/client/controllers/replica.ts
+++ src/client/controllers/replica.ts
@@ -1,24 +1,18 @@
 import { Controller, type OnStart } from "@flamework/core";
 
-import { type PlayerData, INITIAL_DATA } from "shared/structs/player-data";
-import Signal from "@rbxts/lemon-signal";
 import { Message, messaging } from "shared/messaging";
+import { type PlayerData, INITIAL_DATA } from "shared/structs/player-data";
 
-@Controller({ loadOrder: -500 })
+@Controller()
 export class ReplicaController implements OnStart {
-  public readonly loaded = new Signal;
-  public readonly isLoaded: boolean = false;
   public readonly data = INITIAL_DATA;
 
   public onStart(): void {
     messaging.client.on(Message.DataUpdated, data => this.onDataUpdate(data));
+    messaging.server.emit(Message.InitializeData);
   }
 
   public onDataUpdate(data: PlayerData): void {
     (this as { data: PlayerData }).data = data;
-    if (!this.isLoaded) {
-      this.loaded.Fire();
-      (this as { isLoaded: boolean }).isLoaded = true;
-    }
   }
 }
