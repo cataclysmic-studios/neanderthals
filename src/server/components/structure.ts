23829f5224e91e4e8f1c551e01c1f8ec99414657
R-unic <roleyspersonal@gmail.com>
2025-07-14 11:17:02 -0400

    feat: shake animation when structure is hit

--- src/server/components/structure.ts
+++ src/server/components/structure.ts
@@ -1,6 +1,7 @@
 import type { OnStart } from "@flamework/core";
 import { Component } from "@flamework/components";
 import { Players, Workspace as World } from "@rbxts/services";
+import { TweenBuilder } from "@rbxts/twin";
 import { Trash } from "@rbxts/trash";
 import { getDescendantsOfType } from "@rbxts/instance-utility";
 import { $nameof } from "rbxts-transform-debug";
@@ -12,6 +13,10 @@ import { CreatesDropsComponent } from "server/base-components/creates-drops";
 import type { LevelsService } from "server/services/levels";
 
 const DEFAULT_RESPAWN_TIME = 60;
+const SHAKE_TWEEN_INFO = new TweenInfo(0.04, Enum.EasingStyle.Sine, Enum.EasingDirection.In, 0, true);
+const SHAKE_MAGNITUDE = 0.25;
+const SHAKE_LEFT_OFFSET = new Vector3(-SHAKE_MAGNITUDE, 0, -SHAKE_MAGNITUDE);
+const SHAKE_RIGHT_OFFSET = new Vector3(SHAKE_MAGNITUDE, 0, SHAKE_MAGNITUDE);
 
 interface PartInfo {
   readonly collisions: boolean;
@@ -26,6 +31,8 @@ export class Structure extends CreatesDropsComponent<{}, StructureModel> impleme
   public readonly config = require<StructureConfig>(this.instance.Config);
 
   private readonly aliveTrash = new Trash;
+  private readonly root = this.instance.PrimaryPart!;
+  private shakeTween?: Tween;
   private parts = getDescendantsOfType(this.instance, "BasePart");
   private originalPartInfo = new Map<BasePart, PartInfo>;
   private alive = false;
@@ -49,8 +56,10 @@ export class Structure extends CreatesDropsComponent<{}, StructureModel> impleme
     let damage = current;
     if (toolKind !== undefined && toolKind !== requiredToolKind)
       damage /= 2;
-    if (toolTier < minimumToolTier)
+    if (toolTier < minimumToolTier) {
       damage = 0;
+      this.shake(); // dumbest hack lol
+    }
 
     return damage;
   }
@@ -59,17 +68,20 @@ export class Structure extends CreatesDropsComponent<{}, StructureModel> impleme
     if (!this.alive) return;
     this.alive = false;
     this.aliveTrash.purge();
+    this.shakeTween?.Destroy();
+    this.shakeTween = undefined;
 
     const { drops, xp, noRespawn = false, respawnTime = DEFAULT_RESPAWN_TIME } = this.config;
     this.createDrops(drops);
-    this.toggleVisibility(false);
 
     const killerID = this.instance.Humanoid.GetAttribute<number>("AttackerID");
     const killer = killerID !== undefined ? Players.GetPlayerByUserId(killerID) : undefined;
     if (killer !== undefined && xp !== undefined)
       this.levels.addXP(killer, xp);
 
-    if (noRespawn) {
+    if (!noRespawn)
+      this.toggleVisibility(false);
+    else {
       this.parts = [];
       this.originalPartInfo = new Map;
       task.delay(0.25, () => {
@@ -88,8 +100,15 @@ export class Structure extends CreatesDropsComponent<{}, StructureModel> impleme
     this.alive = true;
 
     const humanoid = this.instance.Humanoid;
+    let lastHealth = humanoid.Health;
     this.aliveTrash.add(humanoid.GetPropertyChangedSignal("Health").Connect(() => {
-      if (humanoid.Health > 0) return;
+      const health = humanoid.Health;
+      if (health < lastHealth) {
+        this.shake();
+        lastHealth = health;
+      }
+
+      if (health > 0) return;
       this.kill();
     }));
 
@@ -97,6 +116,22 @@ export class Structure extends CreatesDropsComponent<{}, StructureModel> impleme
       this.toggleVisibility(true);
       humanoid.Health = humanoid.MaxHealth;
     }
+  }
+
+  private shake(): void {
+    const origin = this.root.CFrame;
+    this.shakeTween?.Destroy();
+    this.shakeTween = TweenBuilder.forModel(this.instance)
+      .info(SHAKE_TWEEN_INFO)
+      .property("Value", origin.add(SHAKE_LEFT_OFFSET))
+      .onCompleted(() => {
+        this.shakeTween = TweenBuilder.forModel(this.instance)
+          .info(SHAKE_TWEEN_INFO)
+          .property("Value", origin.add(SHAKE_RIGHT_OFFSET))
+          .onCompleted(() => this.shakeTween = undefined)
+          .play();
+      })
+      .play();
   }
 
   private toggleVisibility(on: boolean): void {
