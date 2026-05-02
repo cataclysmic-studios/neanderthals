c3928b6c899324bf5bbe42f515105cc0297879d5
R-unic <roleyspersonal@gmail.com>
2026-05-02 17:27:42 -0400

    fix: day/night cycle dt issues

--- src/server/services/day-night.ts
+++ src/server/services/day-night.ts
@@ -3,9 +3,11 @@ import { Lighting } from "@rbxts/services";
 
 import type { OnFixed } from "shared/hooks";
 
+const { min } = math;
+
 @Service()
 export class DayNightService implements OnFixed {
   public onFixed(dt: number): void {
-    Lighting.ClockTime += dt / 40;
+    Lighting.ClockTime += min(dt, 1) / 40;
   }
 }
