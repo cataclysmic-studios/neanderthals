ecee40812d84e846b59850e5df391834e8cad8a1
R-unic <roleyspersonal@gmail.com>
2026-05-02 19:55:57 -0400

    perf: optimize item drops

--- src/server/utility.ts
+++ src/server/utility.ts
@@ -27,24 +27,27 @@ export function dropItem(itemTemplate: PVInstance, origin: CFrame, radius: Vecto
   const droppedIDs: number[] = [];
   for (const _ of $range(1, amount)) {
     const id = cumulativeDropID++;
-    const drop = itemTemplate.Clone();
-    const x = radius.X / 2;
-    const y = min(radius.Y / 2, 5);
-    const z = radius.Z / 2;
-    const offset = vector.create(random(-x, x), random(-y, y), random(-z, z));
-
-    drop.SetAttribute("DropID", id);
-    drop.PivotTo(origin.add(offset));
-    drop.Destroying.Once(() => cumulativeDropID = clamp(id - 1, 0, 255));
-    drop.Parent = World.DroppedItems;
-    drop.AddTag("DroppedItem");
     droppedIDs.push(id);
 
-    for (const part of getDescendantsOfType(drop, "BasePart")) {
-      if (part.GetAttribute("UseDefaultDroppedCollisions") === true) continue;
-      part.CanCollide = true;
-      part.CollisionGroup = "DroppedItems";
-    }
+    task.spawn(() => {
+      const drop = itemTemplate.Clone();
+      const x = radius.X / 2;
+      const y = min(radius.Y / 2, 5);
+      const z = radius.Z / 2;
+      const offset = vector.create(random(-x, x), random(-y, y), random(-z, z));
+
+      drop.SetAttribute("DropID", id);
+      drop.PivotTo(origin.add(offset));
+      drop.Destroying.Once(() => cumulativeDropID = clamp(id - 1, 0, 255));
+      drop.Parent = World.DroppedItems;
+      drop.AddTag("DroppedItem");
+
+      for (const part of getDescendantsOfType(drop, "BasePart")) {
+        if (part.GetAttribute("UseDefaultDroppedCollisions") === true) continue;
+        part.CanCollide = true;
+        part.CollisionGroup = "DroppedItems";
+      }
+    });
   }
 
   return droppedIDs;
