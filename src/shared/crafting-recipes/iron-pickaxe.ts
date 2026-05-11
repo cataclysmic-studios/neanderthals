cfb5702dc46e5d71565c79b7e551d8b14cded1b9
R-unic <roleyspersonal@gmail.com>
2026-05-11 00:39:35 -0400

    feat: iron tools require level 8

--- src/shared/crafting-recipes/iron-pickaxe.ts
+++ src/shared/crafting-recipes/iron-pickaxe.ts
@@ -1,9 +1,11 @@
 import { ItemID } from "shared/item-id";
+import { IRON_REQUIRED_LEVEL } from "shared/constants";
 import { RecipeKind, type CraftingRecipe } from "shared/structs/crafting-recipe";
 
 export = {
   kind: RecipeKind.Item,
   yield: ItemID.IronPickaxe,
+  requiredLevel: IRON_REQUIRED_LEVEL,
   ingredients: [
     [ItemID.Stick, 1],
     [ItemID.IronBar, 3]
