6260e17fba5e81e0496d67c10a01a73b5e771e61
R-unic <roleyspersonal@gmail.com>
2026-05-16 01:39:17 -0400

    ref: remove unused utilities

--- src/shared/utility/index.ts
+++ src/shared/utility/index.ts
@@ -1,5 +1,4 @@
 import type { Trash } from "@rbxts/trash";
-import type { HashMap } from "@rbxts/serio";
 
 const { max, floor } = math;
 const { magnitude } = vector;
@@ -35,39 +34,12 @@ export function weldTool(toolTemplate: ToolItem, character: CharacterModel, tras
   return tool;
 }
 
-export function objectFromEntries<K extends string | number | symbol, V>(entries: [K, V][]): Record<K, V> {
-  return new Map(entries) as never; // goat hack
-}
-
 export function distanceBetween(a: Vector3, b: Vector3): number {
   return magnitude(a.sub(b));
 }
 
 export function lerp(a: number, b: number, t: number): number {
   return a + (b - a) * t;
-}
-
-/**
- * Returns a new record with key-value pairs from `record1` that don't exist in `record2`,
- * or where the values differ (when using `compareValues` parameter).
- *
- * @example
- * recordDiff({ a: 1, b: 2 }, { b: 3, c: 4 }) // { a: 1, b: 2 }
- */
-export function recordDiff<K extends string | number | symbol, V>(
-  record1: Record<K, V> | Map<K, V> | HashMap<K, V>,
-  record2: Record<K, V> | Map<K, V> | HashMap<K, V>,
-  compareValues = true
-): Record<K, V> {
-  record1 = record1 as Record<K, V>;
-  record2 = record2 as Record<K, V>; // silly hack for maps
-
-  const result = {} as Record<K, V>;
-  for (const [key] of record1 as Map<K, V>) // silly hack for iteration
-    if (!(key in record2) || compareValues && record1[key] !== record2[key])
-      result[key] = record1[key];
-
-  return result;
 }
 
 export function isNaN(n: number): boolean {
